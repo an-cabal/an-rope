85a69b5569bcdbf94580a3d828a36a429fa0860b
Eliza Weisman <hi@hawkweisman.me>
2016-12-19 12:06:22 -0500

    rope: update tests for changed Rope.rebalance()

--- an-rope/src/test.rs
+++ an-rope/src/test.rs
@@ -3,32 +3,33 @@ use std::iter;
 
 #[test]
 fn rebalance_test_1() {
-    let r = Rope::from("This is a large string \
+    let mut r = Rope::from("This is a large string \
                         that will need to be rebalanced.".to_string());
-    let r = r.rebalance();
+    r.rebalance();
     assert!(r.is_balanced());
 }
 
 #[test]
 fn rebalance_test_2() {
-    let r = Rope::from("Lorem ipsum dolor sit amet, consectetur adipiscing eli\
-                        t, sed do eiusmod tempor incididunt ut labore et dolor\
-                        e magna aliqua. Ut enim ad minim veniam, quis nostrud \
-                        exercitation ullamco laboris nisi ut aliquip ex ea com\
-                        modo consequat. Duis aute irure dolor in reprehenderit\
-                         in voluptate velit esse cillum dolore eu fugiat nulla\
-                         pariatur. Excepteur sint occaecat cupidatat non proid\
-                        ent, sunt in culpa qui officia deserunt mollit anim id\
-                         est laborum.".to_string());
-    let r = r.rebalance();
+    let mut r =
+        Rope::from("Lorem ipsum dolor sit amet, consectetur adipiscing eli\
+                    t, sed do eiusmod tempor incididunt ut labore et dolor\
+                    e magna aliqua. Ut enim ad minim veniam, quis nostrud \
+                    exercitation ullamco laboris nisi ut aliquip ex ea com\
+                    modo consequat. Duis aute irure dolor in reprehenderit\
+                     in voluptate velit esse cillum dolore eu fugiat nulla\
+                     pariatur. Excepteur sint occaecat cupidatat non proid\
+                    ent, sunt in culpa qui officia deserunt mollit anim id\
+                     est laborum.".to_string());
+    r.rebalance();
     assert!(r.is_balanced());
 }
 
 #[test]
 fn big_rebalance() {
     let s: String = iter::repeat('a').take(10_000).collect();
-    let r = Rope::from(s);
-    let r = r.rebalance();
+    let mut r = Rope::from(s);
+    r.rebalance();
     assert!(r.is_balanced());
 }
 
@@ -66,8 +67,8 @@ fn merge_rebalance_test() {
               est laborum.";
 
      let t = Rope::from(s.to_owned());
-     let u = t + s;
-     let u = u.rebalance();
+     let mut u = t + s;
+     u.rebalance();
      assert!(u.is_balanced());
 }
 
