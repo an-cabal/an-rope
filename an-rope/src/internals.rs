936e080b64651a75dceef42dc3268c5d95c6468c
Eliza Weisman <hi@hawkweisman.me>
2016-12-28 16:59:42 -0500

    rope: add feature flag to enable/disable balancing for test purposes

--- an-rope/src/internals.rs
+++ an-rope/src/internals.rs
@@ -78,12 +78,15 @@ impl Default for Node {
     fn default() -> Self { Node::empty() }
 }
 
+
+#[cfg(features = "rebalance")]
 const FIB_LOOKUP: [usize; 93] = [
  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903, 2971215073, 4807526976, 7778742049, 12586269025, 20365011074, 32951280099, 53316291173, 86267571272, 139583862445, 225851433717, 365435296162, 591286729879, 956722026041, 1548008755920, 2504730781961, 4052739537881, 6557470319842, 10610209857723, 17167680177565, 27777890035288, 44945570212853, 72723460248141, 117669030460994, 190392490709135, 308061521170129, 498454011879264, 806515533049393, 1304969544928657, 2111485077978050, 3416454622906707, 5527939700884757, 8944394323791464, 14472334024676221, 23416728348467685, 37889062373143906, 61305790721611591, 99194853094755497, 160500643816367088, 259695496911122585, 420196140727489673, 679891637638612258, 1100087778366101931, 1779979416004714189, 2880067194370816120, 4660046610375530309, 7540113804746346429 ];
 
 
 /// Returns the _n_th fibonacci number.
 #[inline]
+#[cfg(features = "rebalance")]
 fn fibonacci(n: usize) -> usize {
     if n <= 93 { FIB_LOOKUP[n] }
     else { fibonacci(n - 1) + fibonacci(n - 2) }
@@ -224,11 +227,26 @@ impl Node {
     /// > unbalanced subropes.
     /// – from "Ropes: An Alternative to Strings"
     #[inline]
+    #[cfg(features = "rebalance")]
     pub fn is_balanced(&self) -> bool {
         self.len() >= fibonacci(self.depth() + 2)
         // true
     }
 
+    /// Returns true if this node is balanced
+    ///
+    /// > We define the depth of a leaf to be 0, and the depth of a
+    /// > concatenation to be one plus the maximum depth of its children. Let
+    /// > _Fn_ be the _n_th Fibonacci number. A rope of depth _n_ is balanced if
+    /// > its length is at least _Fn_+2, e.g. a balanced rope of depth 1 must
+    /// > have length at least 2. Note that balanced ropes may contain
+    /// > unbalanced subropes.
+    /// – from "Ropes: An Alternative to Strings"
+    #[inline]
+    #[cfg(not(features = "rebalance"))]
+    pub const fn is_balanced(&self) -> bool {
+        true
+    }
 
     /// Returns the depth in the tree of a node
     #[inline]
